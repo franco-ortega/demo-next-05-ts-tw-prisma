febddcd7846ed8c8415d0fd9ad4191f4b7119bf1
Franco Ortega <francopdx@gmail.com>
2023-06-05 18:08:30 -0700

    mark off completed todo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
-import { getTodos } from '@/utils/getTodos';
+// import { getTodos } from '@/utils/getTodos';
 import { TodoItem } from '@/components/TodoItem';
 import Link from 'next/link';
 import { prisma } from './db';
+
+function getTodos() {
+  return prisma.todo.findMany();
+}
 
 async function toggleTodo(id: string, complete: boolean) {
   'use server';
 
   console.log(id, complete);
+
+  await prisma.todo.update({ where: { id }, data: { complete } });
 }
 
 export default async function Home() {
@@ -27,7 +33,7 @@ export default async function Home() {
       </header>
       <ul className='pl-4'>
         {todos.map((todo) => (
-          <TodoItem {...todo} toggleTodo={toggleTodo} />
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
         ))}
       </ul>
     </>
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -10,12 +10,16 @@ type TodoItemProps = {
 export function TodoItem({ id, title, complete, toggleTodo }: TodoItemProps) {
   return (
     <li className='flex gap-1 items-center'>
-      <input id={id} type='checkbox' className='cursor-pointer peer' />
+      <input
+        id={id}
+        type='checkbox'
+        className='cursor-pointer peer'
+        defaultChecked={complete}
+        onChange={(e) => toggleTodo(id, e.target.checked)}
+      />
       <label
         htmlFor={id}
         className='cursor-pointer peer-checked:line-through peer-checked:text-slate-500'
-        defaultChecked={complete}
-        onChange={(e) => toggleTodo(id, e.target.checked)}
       >
         {title}
       </label>
