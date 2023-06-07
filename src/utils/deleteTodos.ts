64123f226a8ffb3e730a450e03c571a3a5b4fdc7
Franco Ortega <francopdx@gmail.com>
2023-06-07 16:31:06 -0700

    clean up code

--- src/utils/deleteTodos.ts
+++ src/utils/deleteTodos.ts
@@ -2,5 +2,6 @@ import { prisma } from "@/app/db";
 
 export async function deleteTodos() {
   'use server'
+  
   return await prisma.todo.deleteMany();
 }
--- src/utils/toggleTodo.tsx
+++ src/utils/toggleTodo.tsx
@@ -3,7 +3,5 @@ import { prisma } from '@/app/db';
 export async function toggleTodo(id: string, complete: boolean) {
   'use server';
 
-  console.log(id, complete);
-
   await prisma.todo.update({ where: { id }, data: { complete } });
 }
