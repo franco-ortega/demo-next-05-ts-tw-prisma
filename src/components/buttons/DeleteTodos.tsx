bf582e23944649ef79e1a1ed93c42859787c852c
Franco Ortega <francopdx@gmail.com>
2023-06-12 16:33:36 -0700

    refactor DeleteTodos styles into object

--- src/components/buttons/DeleteTodos.tsx
+++ src/components/buttons/DeleteTodos.tsx
@@ -6,13 +6,15 @@ type DeleteTodosProps = {
 
 function DeleteTodos({ deleteTodos }: DeleteTodosProps) {
   return (
-    <button
-      onClick={deleteTodos}
-      className='border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none'
-    >
+    <button onClick={deleteTodos} className={styles.button}>
       Delete Todos
     </button>
   );
 }
 
 export default DeleteTodos;
+
+const styles = {
+  button:
+    'border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none',
+};
